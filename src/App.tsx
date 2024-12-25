bb15f8e4746fa3cbb8ab7530b3ea62bd88e4dac8
saqibhussain789 <shkb2003@gmail.com>
2024-12-25 12:56:24 +0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,17 @@ import Footer from "./components/layout/Footer.tsx";
 import Testimonial from "./pages/Testimonials/Testimonial.tsx";
 import CodeComponents from "./pages/CodeComponents/CourseComponents.tsx"
 import Services from "./pages/Services/Services.tsx"
+import Tutorial from "./pages/Tutorials/Tutorials.tsx";
+import Community from "./pages/Community/Community.tsx";
+import Blog from "./pages/Blog/Blog.tsx";
+import Support from "./pages/Support/Support.tsx";
+import LivePreview from "./pages/LivePreview/Livepreview.tsx";
+import Achievements from "./pages/Achievements/Achievements.tsx";
+import Rewards from "./pages/Rewards/Rewards.tsx";
+import Terms from "./pages/Terms&Conditions/Terms.tsx";
+import Privacy from "./pages/PrivacyPolicy/Privacy.tsx";
+
+
 
 const App: React.FC = () => {
   return (
@@ -24,6 +35,15 @@ const App: React.FC = () => {
           <Route path="/testimonials" element={<Testimonial />} />
           <Route path="/code-components" element={<CodeComponents />} />
           <Route path="/services" element={<Services />} />
+          <Route path="/tutorials" element={<Tutorial />} />
+          <Route path="/community-forum" element={<Community />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/support" element={<Support />} />
+          <Route path="/live-preview" element={<LivePreview />} />
+          <Route path="/achievements" element={<Achievements />} />
+          <Route path="/rewards" element={<Rewards />} />
+          <Route path="/terms" element={<Terms />} />
+          <Route path="/privacy-policy" element={<Privacy />} />
 
           {/* Add more routes here */}
         </Routes>
